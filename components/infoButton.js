cb28c51092604cb8246a60fcbb52dfd4bcc0517b
naishalm <naishal1503@gmail.com>
2024-04-22 14:07:18 -0400

    First commit

---
+++ components/infoButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+
+const TempButton = props => {
+  return (
+    <TouchableOpacity
+    onPress={props.onPress} 
+    >
+    <Icon type='font-awesome' name={"info"} size={30} color="#192a56" reverse={true} />
+    </TouchableOpacity>
+  );
+};
+
+export default TempButton;
